0757eed20dd856786eb7be6e33ab2f0473c69e40
dong-fa <120082854+dong-fa@users.noreply.github.com>
2023-07-06 17:24:10 +0900

    회원 가입 기본 폼 제작

--- src/pages/SignPage.jsx
+++ src/pages/SignPage.jsx
@@ -1,5 +1,38 @@
+import React, { useState } from "react";
+
 const SignPage = () => {
-  return <div></div>;
+  const signUserForm = {
+    id: 0,
+    userID: "",
+    userPW: "",
+  };
+  const [userSign, setUserSign] = useState(signUserForm);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setUserSign({ ...userSign, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (userSign.userID.trim() === "" || userSign.userPW.trim() === "") {
+      return alert("내용을 입력하세요");
+    }
+
+    //....데이터 넘기는 작업 필요
+
+    setUserSign(signUserForm); // input 초기화;
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="userID" value={userSign.userID} onChange={onChange} placeholder="Email" />
+        <input type="password" name="userPW" value={userSign.userPW} onChange={onChange} placeholder="Password" />
+        <button>회원가입</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignPage;
